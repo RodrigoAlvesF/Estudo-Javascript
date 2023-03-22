f0c760d57007c26f392b2f99834051de6d2724f1
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-03-22 12:49:47 -0300

    Eventos MOUSE JS 22/03

--- Estudo Javascript/Lógica (básico)/Aula 18 - EVENTOS MOUSE/js.js
+++ Estudo Javascript/Lógica (básico)/Aula 18 - EVENTOS MOUSE/js.js
@@ -1,20 +1,12 @@
-/* Operadores de comparação:
-==: Iguais
-===: Iguais e do mesmo tipo
-!=: Diferentes
-!==: Diferentes e de tipos diferentes
-<: Menor
->: Maior
-<=:Menor ou igual
->=: Maior ou igual
-
-
-*/
-
 /*
 onclick: Aciona quando clicado
 ondblclick: Aciona quando é clicado 2 vezes
 onmouseup: Acionado quando o clique é solto
 onmouseover: Acionado quando cursor passa por cima
 onmouseout: Acionado quando cursor sai de cima
+onmousewheel: Acionado quando scroll utilizado
 */
+
+function acao () {
+    alert("evento disparado")
+}
