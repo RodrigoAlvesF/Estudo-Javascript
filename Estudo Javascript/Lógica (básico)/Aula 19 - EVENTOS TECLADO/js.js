915fe83988bbfd33cf512ee69ed0e5bab8d422d5
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-03-23 19:18:04 -0300

    Estudo JS 23/03

---
+++ Estudo Javascript/Lógica (básico)/Aula 19 - EVENTOS TECLADO/js.js
@@ -0,0 +1,9 @@
+/*
+onkeydown : Acionado quando tecla é preossionada
+onkeypress : Acionado quando CARACTERE for pressionado
+onkeyup : Acionado quando tecla é solta
+*/
+
+function acao () {
+    alert("evento disparado")
+}
