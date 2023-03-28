e1f302139a11d90d058a99d36d8e291c0c445c97
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-03-28 18:17:20 -0300

    Aula arrays básico 28/03

---
+++ Estudo Javascript/Lógica (básico)/Aulas/Aula 26 - Array básico/js.js
@@ -0,0 +1,25 @@
+//Serve para armazenar de forma relacionada qlqr coisa
+
+//Exemplo se formos usar var:
+/*var varivael_1 = 'Banana'
+var varivael_2 = 'Maçã'
+var varivael_3 = 'Morango'
+var varivael_4 = 'Uva'
+*/
+
+
+//Se formos usar Array:
+
+//var lista_frutas = Array()
+
+/*var lista_frutas = [] //Ao usarmos [] já indicamos que é um array
+lista_frutas[1] = 'Banana'
+lista_frutas[2] = true
+lista_frutas['x'] = 'Morango'
+lista_frutas[4] = 100
+*/
+
+var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'] //Também é possível colocar o array na ordem como na linha (indice/chave sempre começaram em 0)
+
+
+console.log (lista_frutas)
