5387be60f29e259e78e501b8f9f17f587017615c
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-03-28 12:06:37 -0300

    App calculadora básica 28/03

--- Estudo Javascript/Lógica (básico)/Desafios/Desafio 05 - DOM input/js.js
+++ Estudo Javascript/Lógica (básico)/Desafios/Desafio 05 - DOM input/js.js
@@ -1,15 +1,13 @@
 function holder(vermelho) {
     document.getElementById('input').style.backgroundColor = vermelho
-}
+} //Função para alterar a cor
 
-function out() {
-    document.getElementById('input')
-}
 
 function out() {
     let out = document.getElementById('input').value
+    //O let serve para que o "out" receba os valores que tem em input
 
-
+    //Para assim testar com o if
     if(out.length < 3) {
         document.getElementById('input').style.background = "red"
         } else {
