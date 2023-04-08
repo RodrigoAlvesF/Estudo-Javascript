c6beb318512a15ec9e10421ed6505b41c58cfd59
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-04-08 15:20:08 -0300

    Atualização mini-game mosquito JS (variações de tamanhos e posições + anotações para entendimento 08/04

--- Estudo Javascript/Projetos/Mata-Mosquito/js.js
+++ Estudo Javascript/Projetos/Mata-Mosquito/js.js
@@ -1,4 +1,8 @@
+
+//Criamos a função para que as variáveis recebam o conteúdo indicado
 function ajustaTamanhoPalcoJogo(){
+    //Window representa o browser, a partir dele temos informações do browser
+    //innerHeight e innerWidth informam altura e largura da janela visíviel
     altura = window.innerHeight
     largura = window.innerWidth
 
@@ -12,9 +16,13 @@ function posicaoRandomica(){
 
     // Math.floor faz o arredondamento para baixo para tirar as casas decimais
     //Usamos o Math.random * o valor que queremos que seja o máximo do random.
+    //Multiplicamos para que tenhamos valores "inteiros" e nunca passando dos limites da janela (necessário a multiplicação para usar o Math.floor)
+    //ultilizamos o "-90" para que a mosca nunca passe da tela, tendo um limite para onde pode se mover
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
+    //o codigo abaixo serve para que, caso o random caia em 0, ele irá calcular -90, o que ficará negativo e sumirá da tela, logo, o código abaixo diz:
+    //se posicao for menor que 0, valerá 0, se não, continuará o que estivervalendo.
     posicaoX = posicaoX < 0 ? 0 : posicaoX 
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
@@ -23,13 +31,41 @@ function posicaoRandomica(){
 
     //Cirar o elemento HTML
     var mosquito = document.createElement('img')
-    mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosca'
+    mosquito.src = 'imagens/mosca.png' // Para aparecer a mosca na função.
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() //A função tamanhAleatório guarda 3 tipos de classes, a função ladoAleatorio guarda 2 classes.
+    //OBS: devemos usar o espaçamento + ' ' + para que ambas funões sejam usadas. 
     mosquito.style.left = posicaoX + 'px'
-    mosquito.style.top = posicaoY + 'px'
-    mosquito.style.position = 'absolute'
+    mosquito.style.top = posicaoY + 'px' //Não esquecer de colocar os px
+    mosquito.style.position = 'absolute' //absolute serve para "soltar" a imagem na tela
 
     //para adicionarmos um elemento dentro de outro elemento, transformamos ele é uma variável
     document.body.appendChild(mosquito)
 
+    
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3) //Multiplicamos para 3 para aumentar o limite máximo
+
+    //Usaremos um switch para tomada de decisão dependendo do resultado
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2) //Multiplicamos para 2 para aumentar o limite máximo
+
+    //Usaremos um switch para tomada de decisão dependendo do resultado
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
