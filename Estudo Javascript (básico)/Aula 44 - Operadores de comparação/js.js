d58ab02332230546127440dd724c0486fb473133
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-05-01 12:13:52 -0300

    Modelo CSS e HTML para futuros exercícios 01/05

---
+++ Estudo Javascript (básico)/Aula 44 - Operadores de comparação/js.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação
+*/
+
+// > Maior que
+// >= Maior que ou igual a
+// < Menor que
+// <= Menor que ou igual a 
+// == Igualdade (valor)
+// === Igualdade estrita (valor e tipo)
+// != diferente (valor)
+// !== diferente estrito (valor e tipo)
---
+++ Estudo Javascript (básico)/Aula 45 - Operadores lógicos/js.js
@@ -0,0 +1,7 @@
+/*
+Operadores lógicos
+
+&& -> AND >- E -> todas as expressôes precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
