281add48e76588ff240b90ace2e55d59aac8b6c4
RodrigoAlvesF <rodrigofidem@gmail.com>
2023-05-15 13:37:56 -0300

    Alterando valores objeto literal 15/05

---
+++ Estudo ES6 (ECMAScript)/Aula 15 Objeto literal alterando valores/js.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    nome: 'Jose',
+    idade: 45
+}
+console.log(pessoa)
+
+//caso precise modificar, fazemos assim:
+
+pessoa.nome = 'Fernanda'
+pessoa.idade = 32
+
+console.log(pessoa)
